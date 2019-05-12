abddeed6a10014f66d80a7efa76dd53904db76e9
zengge2 <2470196899@qq.com>
2019-05-12 19:08:48 +0800

    Added source code

---
+++ app/obj/local/armeabi-v7a/objs/boostpower/main.o.d
@@ -0,0 +1,14 @@
+E:/zengxianxi/androidsdk/ZGQboost/app/obj/local/armeabi-v7a/objs/boostpower/main.o: \
+  E:/zengxianxi/androidsdk/ZGQboost/app/jni/main.cpp \
+  E:/zengxianxi/androidsdk/ZGQboost/app/jni/ioncannon_com_qboost_QboostService.h \
+  E:/zengxianxi/androidsdk/ZGQboost/app/jni/qspower/power.h \
+  E:/zengxianxi/androidsdk/ZGQboost/app/jni\qspower/devices.h \
+  E:/zengxianxi/androidsdk/ZGQboost/app/jni\qspower/types.h
+
+E:/zengxianxi/androidsdk/ZGQboost/app/jni/ioncannon_com_qboost_QboostService.h:
+
+E:/zengxianxi/androidsdk/ZGQboost/app/jni/qspower/power.h:
+
+E:/zengxianxi/androidsdk/ZGQboost/app/jni\qspower/devices.h:
+
+E:/zengxianxi/androidsdk/ZGQboost/app/jni\qspower/types.h:
